e85d473a81c471ceab606c1d7e811b6925739000
Jeongmin Lee <dlwogns9807@naver.com>
2023-03-02 10:56:00 +0900

    Level 8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,9 +15,9 @@ public class Main {
             // 명언 등록
             if(qt.equals("등록")){
                 System.out.print("명언 : ");
-                String talking = sc.next();
+                String talking = sc.next().trim();
                 System.out.print("작가 : ");
-                String author = sc.next();
+                String author = sc.next().trim();
                 book newbook = new book(count, talking, author);
                 System.out.println(count + "번 명언이 등록되었습니다.");
                 bookArrayList.add(newbook);
@@ -33,6 +33,7 @@ public class Main {
                 }
             }
 
+            // 삭제
             else if(qt.contains("삭제")){
                 String findId = qt.split("=")[1];
                 int deleteId = Integer.parseInt(findId);
@@ -59,9 +60,26 @@ public class Main {
                         System.out.println(deleteId + "번 명언이 삭제되었습니다.");
                     }
                 }
+            }
 
+            // 수정
+            else if(qt.contains("수정")){
+                String findId = qt.split("=")[1];
+                int modifyId = Integer.parseInt(findId);
 
+                for(book modifyBook : bookArrayList){
+                    if(modifyBook.getId() == modifyId){
+                        System.out.println("명언(기존) : " + modifyBook.getTalking());
+                        System.out.print("명언 : ");
+                        String newTalking = sc.next();
+                        modifyBook.setTalking(newTalking);
 
+                        System.out.println("작가(기존) : " + modifyBook.getAuthor());
+                        System.out.print("작가 : ");
+                        String newAuthor = sc.next();
+                        modifyBook.setAuthor(newAuthor);
+                    }
+                }
             }
 
             // app 종료
