fcf882ec1f2d9ff3afd092078220cb08e48bafe4
Jeongmin Lee <dlwogns9807@naver.com>
2023-03-02 10:15:48 +0900

    Level 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,16 +10,20 @@ public class Main {
         while(true){
             System.out.printf("명령) ");
             String qt = sc.next();
+
+            // 명언 등록
             if(qt.equals("등록")){
-                System.out.printf("명언 : ");
+                System.out.print("명언 : ");
                 String talking = sc.next();
-                System.out.printf("작가 : ");
+                System.out.print("작가 : ");
                 String author = sc.next();
                 book newbook = new book(count, talking, author);
                 System.out.println(count + "번 명언이 등록되었습니다.");
                 bookArrayList.add(newbook);
                 count++;
             }
+
+            // 목록 출력
             else if(qt.equals("목록")){
                 System.out.println("번호 / 작가 / 명언 ");
                 System.out.println("------------------");
@@ -28,6 +32,16 @@ public class Main {
                 }
             }
 
+            else if(qt.contains("삭제")){
+                String findId = qt.split("=")[1];
+                int deleteId = Integer.parseInt(findId);
+                bookArrayList.remove(bookArrayList.get(deleteId));
+                System.out.println(deleteId + "번 명언이 삭제되었습니다.");
+
+
+            }
+
+            // app 종료
             else if (qt.equals("종료")) {
                 break;
             }
@@ -41,6 +55,19 @@ class book{
     private int id;
     private String talking;
     private String author;
+
+
+    public book(int id, String talking, String author){
+        this.id = id;
+        this.talking = talking;
+        this.author = author;
+
+    }
+
+    boolean deleteBook(int id){
+        System.out.println(id + "번 명언이 삭제되었습니다.");
+        return true;
+    }
 
     public int getId() {
         return id;
@@ -63,12 +90,6 @@ class book{
     }
 
     public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public book(int id, String talking, String author){
-        this.id = id;
-        this.talking = talking;
         this.author = author;
     }
 }
