e999c8e590acaa9a4415f3030732115611c20b9a
Jeongmin Lee <dlwogns9807@naver.com>
2023-03-02 09:34:19 +0900

    Level 5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -5,6 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
         int count = 1;
+        ArrayList<book> bookArrayList = new ArrayList<>();
         while(true){
             System.out.printf("명령) ");
             String qt = sc.next();
@@ -13,10 +15,19 @@ public class Main {
                 String talking = sc.next();
                 System.out.printf("작가 : ");
                 String author = sc.next();
-                new book(count, talking, author);
+                book newbook = new book(count, talking, author);
                 System.out.println(count + "번 명언이 등록되었습니다.");
+                bookArrayList.add(newbook);
                 count++;
             }
+            else if(qt.equals("목록")){
+                System.out.println("번호 / 작가 / 명언 ");
+                System.out.println("------------------");
+                for(book newbook : bookArrayList){
+                    System.out.println(newbook.getId() + " / " + newbook.getAuthor() + " / " + newbook.getTalking());
+                }
+            }
+
             else if (qt.equals("종료")) {
                 break;
             }
@@ -30,6 +41,30 @@ class book{
     private int id;
     private String talking;
     private String author;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTalking() {
+        return talking;
+    }
+
+    public void setTalking(String talking) {
+        this.talking = talking;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
     public book(int id, String talking, String author){
         this.id = id;
